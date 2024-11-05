a2d6d9451765d313dfbac497954242f9111d1aa9
Sufiyan-Project <shaikhsufiyan45792gmail.com>
2024-11-05 10:51:17 +0530

    first commit

---
+++ style.js
@@ -0,0 +1,9 @@
+function automaticallysubmit() {
+    
+
+    document.getElementById('address2').value = document.getElementById('address').value;
+    document.getElementById('city2').value = document.getElementById('city').value;
+    document.getElementById('state2').value = document.getElementById('state').value;
+    document.getElementById('pincode2').value = document.getElementById('pincode').value;
+    document.getElementById('phone2').value = document.getElementById('phone').value;
+}
